e388e9f96273d0e86c6948a4da8384eba8976b1b
m1hanch <misha5264@gmail.com>
2023-07-11 00:39:46 +0200

    entry_points added
    entry_points added, but still got a TypeError: sorting() missing 1 required positional argument: 'directory'

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='goit_hw7',
+setup(name='goit_homework_7',
       version='1',
       description='Example package',
       author='Mykhailo Iereshchenko',
       url='https://github.com/m1hanch',
       license='MIT',
-      packages=find_packages())
+      packages=find_packages(),
+      entry_points={'console_scripts':'hw7 = clean_folder.clean:sorting'})
